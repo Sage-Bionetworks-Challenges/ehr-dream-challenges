3474ec87cb897d851e9b4c280e3921ae17e08bc4
Yao Yan <yy6linda@gmail.com>
2020-07-13 22:54:10 -0700

    update baseline model

--- examples/covid19-question-1/src/train.py
+++ examples/covid19-question-1/src/train.py
@@ -4,9 +4,9 @@ from datetime import date
 import pandas as pd
 import sklearn
 from sklearn.linear_model import LogisticRegressionCV
-from joblib import load
+from joblib import dump
 print("Load measurement.csv", flush = True)
-measurement = pd.read_csv('/infer/measurement.csv',usecols = ['measurement_concept_id','value_as_number','person_id'])
+measurement = pd.read_csv('/train/measurement.csv',usecols = ['measurement_concept_id','value_as_number','person_id'])
 measurement_feature = {'3020891':37.5,'3027018':100,'3012888':80,'3004249':120,
 '3023314':52,'3013650':8,'3004327':4.8,'3016502':95}
 measurement = measurement.dropna(subset = ['measurement_concept_id'])
@@ -47,14 +47,14 @@ condition
 '''
 print("Load condition.csv", flush = True)
 condition_feature = ['254761','437663','378253','259153']
-condition = pd.read_csv("/infer/condition_occurrence.csv",usecols = ['condition_concept_id','person_id'])
+condition = pd.read_csv("/train/condition_occurrence.csv",usecols = ['condition_concept_id','person_id'])
 condition = condition.dropna(subset = ['condition_concept_id'])
 condition = condition.astype({"condition_concept_id": int})
 condition = condition.astype({"condition_concept_id": str})
 for i in condition_feature:
     subm = condition[condition['condition_concept_id'] == i]
     feature[i] = set(subm.person_id)
-person = pd.read_csv('/infer/person.csv')
+person = pd.read_csv('/train/person.csv')
 today = date.today().year
 person['age'] = person['year_of_birth'].apply(lambda x: today - x )
 sub = person[person['age'] > 60]
@@ -70,13 +70,15 @@ for i in feature.keys():
     for person_id in feature[i]:
         index_p = person_index[person_id]
         index_feat_matrix[index_p,index_f] = 1
-print("Feature set for inference dataset is generated", flush = True)
 
-person_id = person[['person_id']]
-clf =  load('/model/baseline.joblib')
-Y_pred = clf.predict_proba(index_feat_matrix)[:,1]
-output = pd.DataFrame(Y_pred,columns = ['score'])
-output_prob = pd.concat([person_id,output],axis = 1)
-output_prob.columns = ["person_id", "score"]
-output_prob.to_csv('/output/predictions.csv', index = False)
-print("Inferring stage finished", flush = True)
+X = index_feat_matrix
+gs = pd.read_csv('/train/goldstandard.csv')
+person_status = person.merge(gs, how = 'left', on = ['person_id'])
+person_status.drop_duplicates(subset=['person_id'], keep = 'first',inplace = True)
+Y =  np.array(person_status[['status']]).ravel()
+print("Y.shape",flush = True)
+print(Y.shape,flush = True)
+clf = LogisticRegressionCV(cv = 10, penalty = 'l2', tol = 0.0001, fit_intercept = True, intercept_scaling = 1, class_weight = None, random_state = None,
+max_iter = 100, verbose = 0, n_jobs = None).fit(X,Y)
+dump(clf, '/model/baseline.joblib')
+print("Training stage finished", flush = True)
